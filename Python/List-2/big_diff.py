8f9b7c8da6e0e456d421cef6af58b640608f0b5c
Adrian Molofsky <molofsky@stanford.edu>
2023-01-09 02:11:46 -0800

    Create big_diff.py

---
+++ Python/List-2/big_diff.py
@@ -0,0 +1,8 @@
+def big_diff(nums):
+  max = nums[0]
+  min = nums[0]
+  
+  for i in range(len(nums)):
+    if (max < nums[i]): max = nums[i]
+    if (min > nums[i]): min = nums[i]
+  return max - min
