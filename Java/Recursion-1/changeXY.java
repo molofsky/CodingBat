5dbde22134fe93f673622156898e8381ae216650
Adrian Molofsky <molofsky@stanford.edu>
2023-08-04 10:23:46 -0700

    Create changeXY.java

---
+++ Java/Recursion-1/changeXY.java
@@ -0,0 +1,5 @@
+public String changeXY(String str) {
+  if (str.length() == 0) return "";
+  if (str.charAt(0) == 'x') return 'y' + changeXY(str.substring(1, str.length()));
+  else return str.charAt(0) + changeXY(str.substring(1, str.length()));
+}
