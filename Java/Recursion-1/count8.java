359794e53ed11e3951be8ac0ea7d37ef3f2b1c0c
Adrian Molofsky <molofsky@stanford.edu>
2023-08-04 10:22:50 -0700

    Create count8.java

---
+++ Java/Recursion-1/count8.java
@@ -0,0 +1,6 @@
+public int count8(int n) {
+  if (n == 0) return 0;
+  if (n % 100 == 88) return 2 + count8(n/10);
+  if (n % 100 != 88 && n % 10 == 8) return 1 + count8(n/10);
+  else return count8(n/10);
+}
