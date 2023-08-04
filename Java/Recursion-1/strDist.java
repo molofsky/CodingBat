33e67782ce9b2dd423c6db810450c8397597400e
Adrian Molofsky <molofsky@stanford.edu>
2023-08-04 10:28:05 -0700

    Create strDist.java

---
+++ Java/Recursion-1/strDist.java
@@ -0,0 +1,11 @@
+public int strDist(String str, String sub) {
+  if (str.length() < sub.length()) return 0;
+  
+  if (!str.substring(0, sub.length()).equals(sub)) {
+      return strDist(str.substring(1, str.length()), sub);
+  }
+  if (!str.substring(str.length() - sub.length()).equals(sub)) {
+      return strDist(str.substring(0, str.length() - 1), sub);
+  }
+  return str.length();
+}
