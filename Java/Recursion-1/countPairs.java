6f1b10e2a9f45cbb4258e663cbf49dd260f84562
Adrian Molofsky <molofsky@stanford.edu>
2023-08-04 10:25:45 -0700

    Create countPairs.java

---
+++ Java/Recursion-1/countPairs.java
@@ -0,0 +1,7 @@
+public int countPairs(String str) {
+  if (str.length() < 3) return 0;
+  if (str.charAt(0) == str.charAt(2)) {
+    return 1 + countPairs(str.substring(1, str.length()));
+  }
+  return countPairs(str.substring(1, str.length()));
+}
