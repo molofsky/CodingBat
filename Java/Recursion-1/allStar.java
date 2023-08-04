60ef9ab965a9f10ba313348cf3de61d082893add
Adrian Molofsky <molofsky@stanford.edu>
2023-08-04 10:25:06 -0700

    Create allStar.java

---
+++ Java/Recursion-1/allStar.java
@@ -0,0 +1,5 @@
+public String allStar(String str) {
+  if (str.length() == 0) return "";
+  if (str.length() != 1) return str.charAt(0) + "*" + allStar(str.substring(1, str.length()));
+  else return str.charAt(0) + allStar(str.substring(1, str.length()));
+}
